88d9128eb6d5992e1a2b33163cb55ab15e460e89
Elijah Zapien <elijah@madeinhaus.com>
2016-02-23 15:04:29 -0800

    Initial Babel setup

---
+++ server/index.js
@@ -0,0 +1,26 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+
+const server = express();
+
+server.use(bodyParser.urlencoded({ extended: true }));
+server.use(bodyParser.json());
+
+const port = process.env.PORT || 8080;
+
+// ROUTES
+// ------
+const router = express.Router();
+
+// (GET http://localhost:8080/api)
+router.get('/', function(req, res) {
+    res.json({ version: '0.0.1' });
+});
+
+server.use('/api', router);
+
+// START SERVER
+// ------------
+server.listen(port);
+console.log('API listening on port ' + port);
+
